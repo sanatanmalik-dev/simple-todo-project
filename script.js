d75ebf5b6584f060f78fb6e00fff828ef8213f9d
Sanatan Malik <sanatan.malik.dev@gmail.com>
2026-01-06 10:27:08 +0530

    Initial commit - Simple ToDo App

---
+++ script.js
@@ -0,0 +1,15 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerText = task;
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
